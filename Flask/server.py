f1c1292eb92c3b310ff14d1051b5492e33c801ff
toybattle <toybattle12@gmail.com>
2026-04-02 11:52:39 +0200

    commit

--- Flask/server.py
+++ Flask/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import random
+import random
 
 app = Flask(__name__)
 
@@ -32,7 +33,11 @@ def test():
 # Créer une partie
 @app.route("/create_game", methods=["POST"])
 def create():
-    game_id = str(uuid.uuid4())
+    game_id = gen_code()
+    map_id = map_choice()
+    games[game_id] = create_game(map_id)
+    return jsonify({"game_id": game_id, "map_id" : map_id})
+    game_id = gen_code()
     games[game_id] = create_game()
     return jsonify({"game_id": game_id})
 
@@ -108,3 +113,16 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
+
+
+
+
+def gen_code():
+    code = ""
+    for i in range(4):
+        code = code + str(random.randint(0,9))
+    return code
+
+def map_choice():
+    # return random.randint(0,7)
+    return random.randint(0,1)
